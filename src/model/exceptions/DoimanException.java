9c7caa6f4a3f59f5cb7fea130322fb793e5efe78
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-07-17 14:04:14 -0300

    Exception Project

---
+++ src/model/exceptions/DoimanException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class DoimanException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DoimanException(String msg) {
+		super(msg);
+	}
+}
